1009459dddea812cd3af1a590f06cc4f1f9d5e50
Taranchuk <nfhfyxer@gmail.com>
2024-04-02 14:45:31 +0300

    base 1.5 update

---
+++ 1.5/Source/VIESAS/HarmonyPatches.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace VIESAS
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyInit
+    {
+        public static Harmony harmonyInstance;
+        static HarmonyInit()
+        {
+            harmonyInstance = new Harmony("VIESAS.Mod");
+            harmonyInstance.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(Pawn_IdeoTracker), "SetIdeo")]
+    public static class SetIdeo_Patch
+    {
+        public static void Postfix(Pawn ___pawn)
+        {
+            if (___pawn.IsColonist)
+            {
+                IdeologyTracker.Instance.RecheckIdeos();
+            }
+        }
+    }
+}
